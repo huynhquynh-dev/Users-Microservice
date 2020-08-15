4dcffdc054d6a2233f2e14c5c12946ce1bc50ccd
huynhquynh-dev <huynhquynh.data@gmail.com>
2020-08-15 21:35:44 +0700

    Update Error Handling with Feign Hystrix FallbackFactory

--- PhotoAppApiUsers/src/main/java/com/huynhquynh/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/huynhquynh/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,6 +2,7 @@ package com.huynhquynh.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,6 +15,7 @@ import feign.Logger;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
